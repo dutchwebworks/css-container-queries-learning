8119802ce3a174123ac37a0eb20f78035306ee77
Dennis Burger <dennis.burger@poort80.nl>
2023-02-15 11:18:39 +0100

    Added scripts, images and manifest for PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js");
+
+workbox.routing.registerRoute(
+  /\.(?:css|js)$/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "assets",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 1800,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|jpg|jpeg|gif|webp)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 1000,
+        maxAgeSeconds: 1800,
+      }),
+    ],
+  })
+);
